6e7972fb09f2ac8499b992002191a610622ce8ec
Shreyas Kothari <spkothari0@gmail.com>
2024-06-11 02:20:07 -0400

    Adding user management with database

--- src/main/java/com/shreyas/spring_boot_demo/config/SecurityConfig.java
+++ src/main/java/com/shreyas/spring_boot_demo/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.shreyas.spring_boot_demo.config;
 
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -9,7 +11,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,6 +28,13 @@ public class SecurityConfig {
     public static final long EXPIRATION_TIME = 864_000_000; // 10 days
     public static final String TOKEN_PREFIX = "Bearer ";
 
+    final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
@@ -31,11 +44,34 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
+    @Bean             // before this once run: Create user schema present in user.sql
     public UserDetailsService userDetailsService() {
-        UserDetails user1= User.withUsername("shreyas").password("{noop}123").roles("USER").build();
-        UserDetails user2= User.withUsername("admin").password("{noop}admin").roles("ADMIN").build();
+        JdbcUserDetailsManager jdbcUserMgr = new JdbcUserDetailsManager(dataSource);
 
-        return new InMemoryUserDetailsManager(user1, user2);
+        if(!userExists(jdbcUserMgr,"shreyas")) {
+            UserDetails user1= User.withUsername("shreyas").password(passwordEncoder().encode("123")).roles("USER").build();
+            jdbcUserMgr.createUser(user1);
+        }
+        if(!userExists(jdbcUserMgr,"admin")) {
+            UserDetails user2 = User.withUsername("admin").password(passwordEncoder().encode("admin")).roles("ADMIN").build();
+            jdbcUserMgr.createUser(user2);
+        }
+
+        return jdbcUserMgr;
+//        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    private boolean userExists(JdbcUserDetailsManager jdbcUserDetailsManager, String username) {
+        try {
+            jdbcUserDetailsManager.loadUserByUsername(username);
+            return true;
+        } catch (UsernameNotFoundException e) {
+            return false;
+        }
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
